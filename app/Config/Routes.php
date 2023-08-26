1f27ebe8d6102356ce84710e1de0c7fe05da832e
Karungg <miftahfadilah71@gmail.com>
2023-08-26 09:14:38 +0700

    pertemuan 1Menentukan route untuk controller contoh1 menampilkan biodata

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,12 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\Contoh1;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Menampikan biodata contoh1
+$routes->get('/contoh1', [Contoh1::class, 'index']);
