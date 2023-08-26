d8d8bca8435acea666e1b8347e450de8e07fdd76
Karungg <miftahfadilah71@gmail.com>
2023-08-26 09:13:20 +0700

    Pertemuan 1 Membuat controller contoh1 menampilkan biodata

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Miftah Fadilah
+        saya tinggal di daerah dramaga
+        olahraga yang saya sukai adalah
+        sepakbola";
+    }
+}
